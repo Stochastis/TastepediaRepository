592e283f37faff6b6b3c4255f3a5466d3c85cd3e
Stochastis <calebsross@gmail.com>
2022-02-14 16:23:38 -0600

    Initial Commit

---
+++ Recipedia/Shared/RecipediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipediaApp.swift
+//  Shared
+//
+//  Created by CSStudent on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

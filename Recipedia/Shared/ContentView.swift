9171d049e36db6f5b912a64fc674676d3ab15783
Stochastis <calebsross@gmail.com>
2022-02-16 18:51:37 -0600

    Background is now orange

--- Recipedia/Shared/ContentView.swift
+++ Recipedia/Shared/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+                Color.orange.edgesIgnoringSafeArea(.all)
+                Text("Hello, world!").padding()
+                }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
+            
     }
 }

4b0aa66376c533c7e8ca58157e8811c05cec6715
Stochastis <calebsross@gmail.com>
2022-02-23 19:46:50 -0600

    Changed stacks around and added top text

--- Recipedia/Shared/ContentView.swift
+++ Recipedia/Shared/ContentView.swift
@@ -9,30 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack() {
-            ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-                VStack {
-                    ForEach(0 ..< 3) { item in
-                        Button(action: {
-                            print("Button 1 Pressed")
-                        }) {
-                            Text("Woa")
-                                .frame(width: 50, height: 50)
-                                .font(.system(size: 18))
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                                )
+        VStack {
+                                        
+            Text("Enter some ingredients.")
+                .alignmentGuide(.top) {dimension in
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/dimension[.top]/*@END_MENU_TOKEN@*/
+                }
+            
+            HStack() {
+                ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                    VStack {
+                        ForEach(0 ..< 3) { item in
+                            Button(action: {
+                                print("Button 1 Pressed")
+                            }) {
+                                Text("Woa")
+                                    .frame(width: 50, height: 50)
+                                    .font(.system(size: 18))
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                                    )
+                            }
+                            .padding(.bottom, 1.0)
                         }
-                        .padding(.bottom, 1.0)
                     }
                 }
             }
-            
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
         .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.orange/*@END_MENU_TOKEN@*/)
-        .ignoresSafeArea(.all)
+        .ignoresSafeArea()
+        
     }
 }
 

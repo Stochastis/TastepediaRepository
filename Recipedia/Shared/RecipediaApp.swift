9850e430d0589fd8b3a765549445bd7085ece04b
Stochastis <calebsross@gmail.com>
2022-03-20 21:16:11 -0500

    Added ingredients list to app bundle.

--- Recipedia/Shared/RecipediaApp.swift
+++ Recipedia/Shared/RecipediaApp.swift
@@ -15,8 +15,9 @@ struct RecipediaApp: App {
     // Create an instance of the Pantry object to keep track of ingredients
     @StateObject var pantry = Pantry(startingIngredients: [])
     
+    // Create an instance of the IngredientsFile object to open up the txt file of ingredients and use them later
     @StateObject var ingredientsList = IngredientsFile()
-            
+    
     // Start the application in the MainView
     var body: some Scene {
         WindowGroup {
@@ -38,19 +39,16 @@ class IngredientsFile: ObservableObject {
     var ingredientsList: [String] = []
     
     init() {
-        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("Top1kIngredients.txt")
-        
-        do {
-            let ingredientsText = try String(contentsOf: path)
-            
-            for line in ingredientsText.split(separator: "\r\n") {
-                ingredientsList.append(String(line))
+        if let fileURL = Bundle.main.url(forResource: "Top1kIngredients", withExtension: "txt") {
+            if let fileContents = try? String(contentsOf: fileURL) {
+                print("Loaded ingredients file from bundle.")
+                for line in fileContents.split(separator: "\r\n") {
+                    ingredientsList.append(String(line))
+                }
+            } else {
+                print("Error loading ingredients file.")
             }
-        } catch {
-            print(error.localizedDescription)
         }
-        print(path.absoluteString)
-        print(getDocumentDirectory())
     }
     
     func getDocumentDirectory() -> URL {

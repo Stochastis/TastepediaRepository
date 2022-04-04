a46a03dc3731b665cf38a1485942dccb73e370c5
Stochastis <59930635+Stochastis@users.noreply.github.com>
2022-04-04 15:29:18 -0500

    Display all steps of recipe instructions.

--- Tastepedia/Shared/RecipeDetailsView.swift
+++ Tastepedia/Shared/RecipeDetailsView.swift
@@ -11,10 +11,22 @@ struct RecipeDetailsView: View {
     
     // Create a View Model to interact with the API
     @StateObject var model: InstructionSearchViewModel
-
+    
     var body: some View {
-        ZStack{
-            Text(model.instructions[0].steps![0].step!)
+        ScrollView {
+            ForEach(0 ..< model.instructions[0].steps!.count, id: \.self) { i in
+                VStack {
+                    Text("Step \(i+1):")
+                    Text(model.instructions[0].steps![i].step!)
+                        .padding(/*@START_MENU_TOKEN@*/[.leading, .bottom, .trailing]/*@END_MENU_TOKEN@*/)
+                }
+            }
         }
+    }
+}
+
+struct RecipeDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeDetailsView(model: InstructionSearchViewModel())
     }
 }

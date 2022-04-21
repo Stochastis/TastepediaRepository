64001dd485e1ef703238727fdec37b30dc078e04
Stochastis <59930635+Stochastis@users.noreply.github.com>
2022-04-21 11:34:57 -0500

    Fraction display implemented.

--- Tastepedia/Views/RecipeDetailsView.swift
+++ Tastepedia/Views/RecipeDetailsView.swift
@@ -23,7 +23,7 @@ struct RecipeDetailsView: View {
             LazyVStack(pinnedViews: .sectionHeaders) {
                 Section(header: Text("Ingredients").frame(width: 350, height: 25, alignment: .center).background(Color.orange)) {
                     ForEach(0 ..< recipe.ingredients.names.count, id: \.self) { i in
-                        Text("\(recipe.ingredients.amounts[i]) \(recipe.ingredients.units[i].capitalized) of \(recipe.ingredients.names[i].capitalized)").frame(width: 350, height: 50, alignment: .leading)
+                        Text("\(formattedAmount(unformatted: recipe.ingredients.amounts[i])) \(recipe.ingredients.units[i].capitalized) of \(recipe.ingredients.names[i].capitalized)").frame(width: 350, height: 50, alignment: .leading)
                     }
                 }
             }
@@ -42,9 +42,6 @@ struct RecipeDetailsView: View {
                     }
                 }
             }.onAppear(perform: {
-                withUnsafePointer(to: recipe) {
-                    print(" This recipe object \(recipe) has address: \($0)")
-                }
                 recipe.getInstructions()
             })
         }.navigationBarTitleDisplayMode(.inline).navigationTitle(recipe.name).toolbar(content: {
@@ -63,4 +60,51 @@ struct RecipeDetailsView: View {
             })
         })
     }
+}
+
+func formattedAmount(unformatted: String) -> String {
+    let wholeDouble = Double(unformatted)?.rounded(.down)
+    let wholeInt = Int(wholeDouble!)
+    var fraction = ""
+    switch Double(unformatted)?.truncatingRemainder(dividingBy: 1.0) {
+    case 0.1:
+        fraction = "1/10"
+    case 0.125:
+        fraction = "1/8"
+    case 0.2:
+        fraction = "1/5"
+    case 0.25:
+        fraction = "1/4"
+    case 0.3:
+        fraction = "3/10"
+    case 1/3:
+        fraction = "1/3"
+    case 0.375:
+        fraction = "3/8"
+    case 0.4:
+        fraction = "2/5"
+    case 0.5:
+        fraction = "1/2"
+    case 0.6:
+        fraction = "3/5"
+    case 0.625:
+        fraction = "5/8"
+    case 2/3:
+        fraction = "2/3"
+    case 0.7:
+        fraction = "7/10"
+    case 0.75:
+        fraction = "3/4"
+    case 0.8:
+        fraction = "4/5"
+    case 0.875:
+        fraction = "7/8"
+    case 0.9:
+        fraction = "9/10"
+    case .none:
+        fraction = ""
+    case .some(_):
+        fraction = ""
+    }
+    return (wholeInt == 0) ? fraction : String(String(wholeInt) + " " + fraction)
 }
